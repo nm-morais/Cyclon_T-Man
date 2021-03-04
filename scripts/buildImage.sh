3e4d74ddfae58b693a12230dff1974d69ac41bc6
Nuno Morais <nm.morais@campus.fct.unl.pt>
2021-03-04 19:29:34 +0000

    changes to  adapt to automation scripts

--- scripts/buildImage.sh
+++ scripts/buildImage.sh
@@ -5,4 +5,6 @@ if [ -z $DOCKER_IMAGE ]; then
   exit
 fi
 
-docker build --build-arg LATENCY_MAP=$LATENCY_MAP --build-arg IPS_FILE=$IPS_FILE --file build/test.Dockerfile . -t $DOCKER_IMAGE
+(cd ../go-babel && ./scripts/buildImage.sh)
+
+docker build --build-arg LATENCY_MAP=$LATENCY_MAP --build-arg IPS_FILE=$IPS_FILE --file build/Dockerfile . -t $DOCKER_IMAGE
--- scripts/setupContainers.sh
+++ scripts/setupContainers.sh
@@ -37,73 +37,26 @@ if [[ $n_nodes -eq 0 ]]; then
   exit
 fi
 
-echo "number of nodes: $n_nodes"
-
-currdir=$(pwd)
-delete_containers_cmd='docker ps -a | awk '{ print $1,$2 }' | grep $DOCKER_IMAGE | awk '{print $1 }' | xargs -I {} docker rm -f {}'
-build_cmd="export export LATENCY_MAP=$LATENCY_MAP; export IPS_FILE=$IPS_FILE export DOCKER_IMAGE=$DOCKER_IMAGE; cd ${currdir}; ./scripts/buildImage.sh"
-delete_logs_cmd="docker run --rm -v $SWARM_VOL:/data bash sh -c 'rm -rf /data/*'"
-host=$(hostname)
-for node in $@; do
-  {
-    echo "deleting running containers on node: $node" 
-    oarsh $node "$delete_containers_cmd"
-    echo "done deleting containers on node: $node!"  
-  } &
-done
-wait
-sleep 1s
-
-# FOR USE IN NAS
-docker run --rm -v $SWARM_VOL:/data bash sh -c 'rm -rf /data/*' || true
-# UNCOMMENT FOR LOCAL FILES ON NODES
-# for node in $@; do
-#   {
-#     echo "deleting logs on node: $node" 
-#     oarsh $node "$delete_logs_cmd" 
-#     echo "done deleting logs on node: $node!"  
-#     wait
-#   } &
-# done
-# wait
-# sleep 1s
-
-for node in $@; do
-  {
-    echo "starting build on node: $node" 
-    oarsh $node $build_cmd 
-    echo "done building image on node: $node!" 
- } &
-done
-wait
-sleep 1s
-echo "Deploying with config file:"
-cat $IPS_FILE
-
-maxcpu=$(nproc)
-nContainers=$(wc -l $IPS_FILE)
 i=0
-
+echo "number of nodes: $n_nodes"
 echo "Lauching containers..."
-while read -r ip
+while read -r ip name
 do
   echo "ip: $ip"
   echo "name: $name"
   idx=$(($i % n_nodes))
   idx=$((idx+1))
   node=${!idx}
-  name="node$i"
 
   cmd="docker run -v $SWARM_VOL:/tmp/logs -d -t --cap-add=NET_ADMIN \
    --net $SWARM_NET \
    --ip $ip \
    --name $name \
-   -h $name \
     $DOCKER_IMAGE $i $nContainers"
 
   # echo "running command: '$cmd'"
 
   echo "Starting ${i}. Container $name with ip $ip and name $name on: $node"
-  oarsh -n $node "$cmd"
+  ssh -n $node "$cmd"
   i=$((i+1))
 done < "$IPS_FILE"
